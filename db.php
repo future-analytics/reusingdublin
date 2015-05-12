c0a26a489cf09d103bbc033624f3b403d0d51db2
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-12 18:19:57 +0100

    build gitignore, footer.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 //creating coonection elements with the database
- $con=mysqli_connect("127.0.0.1","root","password","reusingdublin");
+ $con=mysqli_connect("127.0.0.1","root","chanman","reusingdublin");
  if (mysqli_connect_errno()) {
  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @author  daithi coombes <david.coombes@futureanalytics.ie>
+ */
+?>
+
+		<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-59451880-1', 'auto');
+		ga('send', 'pageview');
+		</script>
+		
+		<div class="container" id="mail" style="padding-top:60px;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" align="center">
+			<p>Subscribe to our newsletter email to get notification about</br>
+				upcoming news, latest project activities and much more!
+			</p>
+		<div align="justify" style="width:100%">
+			<input type="email" class="abc" name='email' style="background-color:#00afc9;width:58%;height:40px;float:left;border:none;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" id='email1' placeholder="Your Email Address">
+			<input type="submit" id="subscribe" style="background-color:#F00;color:white;height:40px;border:none;font-family:'Source Sans Pro', sans-serif;font-size:17px;font-weight:600;width:41%;margin-left:-1px;" name="added" class="subS" value="SUBSCRIBE" onClick="javascript:ValidateEmail();"/>
+		</div>
+		<div style="width:100%;padding:5%;">
+			<img  src="reusing-drraft-13 04-03.png" style="height:auto;width:25%;float:!important;margin-left:1%;">
+			<img  src="Eu_logo1.png" style="height:auto;width:25%;float:!important;margin-left:1%;" >
+			<img  src="images/turas.png"  style="height:auto;width:25%;float:!important;margin-left:1%;">
+		</div>
+		</div>
+		<!-- Bootstrap core JavaScript--->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+	</body>
+</html>
