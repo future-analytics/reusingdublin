a641eb9f7721efec5ee899e2b87feae5a2fae5a8
unknown <Priyanka.singh@FacPriyanka.dhpltd.local>
2015-05-13 10:29:30 +0100

    Signed-off-by: unknown <Priyanka.singh@FacPriyanka.dhpltd.local>

--- includes/header.php
+++ includes/header.php
@@ -2,13 +2,8 @@
 <nav class="navbar navbar-default navbar-fixed-top" style="background-color:#00afc9;border:none;width:100%;">
 <div class="container" >
   <div class="navbar-header" >
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" >
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a onClick="cll();">  <img src="reusing-drraft-13.04-04.png"  style="margin-top:7%;height:37%;width:37%;border-top:hidden;" /></a>
+
+    <a onClick="cll();">  <img src="reusing-drraft-13.04-04.png"  style="margin-top:10%;height:37%;width:37%;border-top:hidden;" /></a>
   </div>
   </div>
   <div class="container">
@@ -19,8 +14,8 @@
         <li><a href="#menu3" id="seconda" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">TRY IT OUT</a></li>
         <li><a href="#menu4" id="thirda"  style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">ABOUT</a></li>
         <li><a href="#menu5" id="fourtha"  style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">MAILING LIST</a></li>
-        <li><a href="https://instagram.com/reusingdublin/" target="_blank"><img href="www.instagram.com" src="instagram19.png"/></a></li>
-        <li><a href="reusingdublin@gmail.com" target="_blank"><img href="reusingdublin@gmail.com"  src="email131.png"/></a></li>
+        <li><a href="https://instagram.com/reusingdublin/" target="_blank"><img src="instagram19.png"/></a></li>
+        <li><a href="mailto:reusingdublin@gmail.com" target="_blank"><img src="email131.png"/></a></li>
         <li><a href="https://www.facebook.com/reusingdublin/" target="_blank"><img href="https://www.facebook.com/reusingdublin/" src="facebook.png"/></a></li>
         <li><a href="https://www.twitter.com/reusingdublin/" target="_blank"><img href="https://www.twitter.com/reusingdublin/"  src="twitter.png"/></a></li>
     </ul>
