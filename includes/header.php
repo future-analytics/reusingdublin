dd2f4fbd163dcdeb22b1c9efc0c88e70b6fcf722
unknown <Priyanka.singh@FacPriyanka.dhpltd.local>
2015-05-13 10:48:28 +0100

    Signed-off-by: unknown <Priyanka.singh@FacPriyanka.dhpltd.local>

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,14 @@
 <div style="background-color:#00afc9;">
-<nav class="navbar navbar-default navbar-fixed-top" style="background-color:#00afc9;border:none;width:100%;">
+<nav class="navbar navbar-default navbar-fixed-top" style="background-color:#00afc9;border:none;width:100%;height:30%">
 <div class="container" >
   <div class="navbar-header" >
 
     <a onClick="cll();">  <img src="reusing-drraft-13.04-04.png"  style="margin-top:10%;height:37%;width:37%;border-top:hidden;" /></a>
   </div>
   </div>
-  <div class="container">
+  <div class="container" >
   <div id="navbar" class="navbar-collapse collapse" style="float:right;">
-    <ul class="nav navbar-nav">
+    <ul class="nav navbar-nav" style="margin-top:-5%;">
         <li><a href="#menu1" id="fiftha" class="active" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">HOME</a></li>
         <li><a href="#menu2" id="firsta" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">HOW IT WORKS</a></li>
         <li><a href="#menu3" id="seconda" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">TRY IT OUT</a></li>
