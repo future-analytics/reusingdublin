8497f0dbc9a71c340647ee50e4afeb1ca5759994
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-14 15:59:19 +0100

    refactor header to bootstrap standard (not completed)
    Signed-off-by: Daithi Coombes <david.coombes@futureanalytics.ie>

--- includes/header.php
+++ includes/header.php
@@ -1,26 +1,65 @@
-<div style="background-color:#00afc9;">
-<nav class="navbar navbar-default navbar-fixed-top" style="background-color:#00afc9;border:none;width:100%">
-<div class="container" >
-  <div class="navbar-header" >
+<header class="reusingdublin">
+    <div class="container">
+    <div class="row">
+        <div class="col-xs-3">
+            <img src="reusing-drraft-13.04-04.png" id="logo" alt="Reusing Dublin Logo">
+        </div>
+        <div class="col-xs-9">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>                        
+                </button>
+            </div>
 
-    <a onClick="cll();">  <img src="reusing-drraft-13.04-04.png"  style="margin-top:10%;height:37%;width:37%;border-top:hidden;" /></a>
-  </div>
-  </div>
-  <div class="container" >
-  <div id="navbar" class="navbar-collapse collapse" style="float:right;">
-    <ul class="nav navbar-nav" style="margin-top:-4%;">
-        <li><a href="#menu1" id="fiftha" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">HOME</a></li>
-        <li><a href="#menu2" id="firsta" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">HOW IT WORKS</a></li>
-        <li><a href="#menu3" id="seconda" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">TRY IT OUT</a></li>
-        <li><a href="#menu4" id="thirda"  style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">ABOUT</a></li>
-        <li><a href="#menu5" id="fourtha"  style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">MAILING LIST</a></li>
-        <li><a href="https://instagram.com/reusingdublin/" target="_blank"><img src="instagram19.png"/></a></li>
-        <li><a href="mailto:reusingdublin@gmail.com" target="_blank"><img src="email131.png"/></a></li>
-        <li><a href="https://www.facebook.com/reusingdublin/" target="_blank"><img href="https://www.facebook.com/reusingdublin/" src="facebook.png"/></a></li>
-        <li><a href="https://www.twitter.com/reusingdublin/" target="_blank"><img href="https://www.twitter.com/reusingdublin/"  src="twitter.png"/></a></li>
-    </ul>
-  </div><!--/.nav-collapse -->
-  </div>
+            <div class="collapse navbar-collapse">
+
+                <ul class="nav navbar-nav page-links">
+                    <li><a href="#menu1" id="fiftha">HOME</a></li>
+                    <li><a href="#menu2" id="firsta">HOW IT WORKS</a></li>
+                    <li><a href="#menu3" id="seconda">TRY IT OUT</a></li>
+                    <li><a href="#menu4" id="thirda">ABOUT</a></li>
+                    <li><a href="#menu5" id="fourtha">MAILING LIST</a></li>
+                </ul>
+                <ul class="nav navbar-nav social-links">
+                    <li><a href="https://instagram.com/reusingdublin/" target="_blank"><img src="instagram19.png"/></a></li>
+                    <li><a href="mailto:reusingdublin@gmail.com" target="_blank"><img src="email131.png"/></a></li>
+                    <li><a href="https://www.facebook.com/reusingdublin/" target="_blank"><img href="https://www.facebook.com/reusingdublin/" src="facebook.png"/></a></li>
+                    <li><a href="https://www.twitter.com/reusingdublin/" target="_blank"><img href="https://www.twitter.com/reusingdublin/"  src="twitter.png"/></a></li>
+                </ul>
+            </div>
+        </nav>
+        </div>
+    </div>
+    </div>
+</header>
+
+<!--
+<div style="background-color:#00afc9;">
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color:#00afc9">
+        <div class="container" >
+            <div class="navbar-header" >
+                <a onClick="cll();"> <img src="reusing-drraft-13.04-04.png"  style="margin-top:10%;height:37%;width:37%;border-top:hidden;"></a>
+            </div>
+        </div>
+            <div class="container" >
+                <div id="navbar" class="navbar-collapse collapse" style="float:right;">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#menu1" id="fiftha" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">HOME</a></li>
+                        <li><a href="#menu2" id="firsta" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">HOW IT WORKS</a></li>
+                        <li><a href="#menu3" id="seconda" style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">TRY IT OUT</a></li>
+                        <li><a href="#menu4" id="thirda"  style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">ABOUT</a></li>
+                        <li><a href="#menu5" id="fourtha"  style="color:#000;font-family:'Source Sans Pro', sans-serif;font-size:19px;">MAILING LIST</a></li>
+                        <li><a href="https://instagram.com/reusingdublin/" target="_blank"><img src="instagram19.png"/></a></li>
+                        <li><a href="mailto:reusingdublin@gmail.com" target="_blank"><img src="email131.png"/></a></li>
+                        <li><a href="https://www.facebook.com/reusingdublin/" target="_blank"><img href="https://www.facebook.com/reusingdublin/" src="facebook.png"/></a></li>
+                        <li><a href="https://www.twitter.com/reusingdublin/" target="_blank"><img href="https://www.twitter.com/reusingdublin/"  src="twitter.png"/></a></li>
+                    </ul>
+                </div><!--/.nav-collapse
+            </div>
+        </div>
+    </nav>
 </div>
-</nav>
-</div>
+-->
