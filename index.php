9413f1b5986773381d264cc7c499aeb7887e0a8f
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-14 09:54:09 +0100

    hack: fix footer

--- index.php
+++ index.php
@@ -1274,32 +1274,6 @@ Resilience and Sustainability).
 
     </div>
  </section>
-<<<<<<< HEAD
 
 <?php
 require_once('includes/footer.php');
-=======
-
-
- <section id="menu5" class="container" id="mail" style="padding-top:60px;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" align="center">
-      <p style="width:100%;margin-left:7%;" align="center;">Subscribe to our newsletter email to get notification about upcoming news, latest project activities and much more!</p>
-<div align="justify" style="width:100%">
-<input type="email" class="abc" name='email' style="background-color:#00afc9;width:58%;height:40px;float:left;border:none;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" id='email1' placeholder="Your Email Address">
-                        <input type="submit" id="subscribe" style="background-color:#F00;color:white;height:40px;border:none;font-family:'Source Sans Pro', sans-serif;font-size:17px;font-weight:600;width:41%;margin-left:-1px;" name="added" class="subS" value="SUBSCRIBE" onClick="javascript:ValidateEmail();"/>
-                        </div>
-                                            <div style="width:100%;padding:5%;">       <img  src="reusing-drraft-13 04-03.png" style="height:13%;width:13%;margin-left:12%;"><img  src="Eu_logo1.png" style="height:8%;width:25%;float:!important;margin:0 5%;" ><img  src="images/turas.png"  style="height:17%;width:17%;float:!important;margin-left:1%;"></div>
-
-
-
- </section>
-
-    <!-- Bootstrap core JavaScript--->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
-    <div class="container" style="margin-bottom:0%;float:center;" align="justify" >
-    <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Reusing Dublin</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
-  </body>
-</html>
->>>>>>> master
