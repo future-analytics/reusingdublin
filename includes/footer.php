39d250674bea366785d0e2a57cbf4eca03126023
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-14 09:54:09 +0100

    hack: fix footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,27 @@
 <?php
 /**
- * @author  daithi coombes <david.coombes@futureanalytics.ie>
- */
+* @author  daithi coombes <david.coombes@futureanalytics.ie>
+*/
 ?>
-
-		<section id="menu5" class="container"  style="padding-top:60px;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" align="center"> <p>Subscribe to our mailing list to get notification about</br>
-upcoming news, latest project activities and much more!</p>
-<div align="justify" style="width:100%">
-<input type="email" class="abc" name='email' style="background-color:#00afc9;width:58%;height:40px;float:left;border:none;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" id='email1' placeholder="Your Email Address">
-						<input type="submit" id="subscribe" style="background-color:#F00;color:white;height:40px;border:none;font-family:'Source Sans Pro', sans-serif;font-size:17px;font-weight:600;width:41%;margin-left:-1px;" name="added" class="subS" value="SUBSCRIBE" onClick="javascript:ValidateEmail();"/>
-											</div>
-																							<div style="width:100%;padding:5%;">       <img  src="reusing-drraft-13 04-03.png" style="height:13%;width:13%;float:!important;margin-left:1%;"><img  src="Eu_logo1.png" style="width:10%;float:!important; margin: 0px 40px;" ><img  src="images/turas.png"  style="height:17%;width:17%;float:!important;margin-left:1%;"></div>
-
-
-
+<section id="menu5" class="container"  style="padding-top:60px;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" align="center">
+	<p>Subscribe to our mailing list to get notification about</br>
+	upcoming news, latest project activities and much more!</p>
+	<div align="justify" style="width:100%">
+		<input type="email" class="abc" name='email' style="background-color:#00afc9;width:58%;height:40px;float:left;border:none;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" id='email1' placeholder="Your Email Address">
+		<input type="submit" id="subscribe" style="background-color:#F00;color:white;height:40px;border:none;font-family:'Source Sans Pro', sans-serif;font-size:17px;font-weight:600;width:41%;margin-left:-1px;" name="added" class="subS" value="SUBSCRIBE" onClick="javascript:ValidateEmail();"/>
+	</div>
+	<div style="width:100%;padding:5%;text-align:center">
+		<img  src="reusing-drraft-13 04-03.png" style="height:13%;width:13%;float:!important;margin-left:1%;">
+		<img  src="Eu_logo1.png" style="width:10%;float:!important; margin: 0px 100px;" >
+		<img  src="images/turas.png"  style="height:17%;width:17%;float:!important;margin-left:1%;">
+	</div>
 </section>
-
-	<!-- Bootstrap core JavaScript--->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-	<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
-	<div class="container" style="margin-bottom:0%;float:center;" align="justify" >
+<!-- Bootstrap core JavaScript--->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="http://getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+<div class="container" style="margin-bottom:0%;float:center;" align="justify" >
 	<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Reusing Dublin</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>.
 	<script>
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -31,6 +31,5 @@ upcoming news, latest project activities and much more!</p>
 	ga('create', 'UA-59451880-1', 'auto');
 	ga('send', 'pageview');
 	</script>
-
 </body>
 </html>
