e373bb756dae8c2372e7ac252ddda7731ec14f72
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-15 13:09:23 +0100

    basic framework - needed to decouple changes from previous code base (config, autoloader & bootstrap files)
    Signed-off-by: Daithi Coombes <david.coombes@futureanalytics.ie>

---
+++ lib/Config.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Singleton config class.
+ * 
+ * @package ReusingDublin
+ * @link http://github.com/future-analytics/reusingdublin
+ * @author daithi coombes <webeire@gmail.com>
+ */
+class Config
+{
+
+	/** @var array An array of database parameters */
+	protected $db;
+	private static $instance;
+
+    /**
+     * Protected constructor to prevent creating a new instance of the
+     * *Config* via the `new` operator from outside of this class.
+     */
+    public function __construct(array $params)
+    {
+
+    	//set properties
+    	foreach($params as $prop => $value)
+    	{
+    		if (!property_exists($this, $prop))
+    			throw new Exception('ReusingDublin Config::setInstance unkown property: '.$prop);
+    		else
+    			$this->$prop = $value;
+    	}
+
+    	self::$instance = $this;
+    }
+
+	/**
+	 * Get the config singleton instalce
+	 * @return Config Returns config singleton.
+	 */
+    public static function getInstance()
+    {
+    	if(! (self::$instance instanceof self)){
+    		throw new Exception("ReusingDublin Config::getInstance singleton instance not constructed");
+    	}
+    	return self::$instance;
+    }
+
+    /**
+     * Private clone method to prevent cloning of the instance of the
+     * *Config* instance.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Private unserialize method to prevent unserializing of the *Config*
+     * instance.
+     *
+     * @return void
+     */
+    private function __wakeup()
+    {
+    }
+}
