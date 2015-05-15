659b9f2b47156e9f6b9451c8eddd665183bb19f2
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-15 11:54:16 +0100

    decouple part of head to include file. Add php includes to the entry form view file

--- Entry_Form.php
+++ Entry_Form.php
@@ -2,76 +2,72 @@
 <?php
 
 if(isset($_POST['added'])){
-$remove[] = "'";
-$remove[] = '"';
-$remove[] = "-";
-$remove[] = ";";
-$msg = "";
-$con=mysqli_connect("172.16.0.57","u1046393_turas","Soamin123@","db1046393_dublin");
-$latitude = mysqli_real_escape_string($con, $_POST['lat1']);
-$longitude = mysqli_real_escape_string($con, $_POST['lon1']);
-$address = mysqli_real_escape_string($con, $_POST['address']);
-$address = str_replace( $remove, "", $address );
-$comment = mysqli_real_escape_string($con, $_POST['comment']);
-$var = nl2br(htmlspecialchars(stripslashes($comment)));
-$var = str_replace( $remove, "",$var );
-$ans1 = mysqli_real_escape_string($con, $_POST['address1']);
-$ans1 = str_replace( $remove, "", $ans1 );
-$ans2 = mysqli_real_escape_string($con, $_POST['address2']);
-$ans2 = str_replace( $remove, "",$ans2 );
-$ans3 = mysqli_real_escape_string($con, $_POST['address3']);
-$ans3 = str_replace( $remove, "",$ans3 );
-$ans4 = mysqli_real_escape_string($con, $_POST['address4']);
-$ans4 = str_replace( $remove, "", $ans4 );
-$ans5 = mysqli_real_escape_string($con, $_POST['address5']);
-$ans5  = str_replace( $remove, "", $ans5  );
-$ans6 = mysqli_real_escape_string($con, $_POST['address6']);
-$ans6 = str_replace( $remove, "", $ans6 );
-$ans7 = mysqli_real_escape_string($con, $_POST['comments']);
-$var1 = nl2br(htmlspecialchars(stripslashes($ans7)));
-$var1  = str_replace( $remove, "", $var1  );
-$ip= mysqli_real_escape_string($con, $_POST['ip']);
-$entry_date = mysqli_real_escape_string($con, $_POST['date1']);
+    $remove[] = "'";
+    $remove[] = '"';
+    $remove[] = "-";
+    $remove[] = ";";
+    $msg = "";
+    $con=mysqli_connect("172.16.0.57","u1046393_turas","Soamin123@","db1046393_dublin");
+    $latitude = mysqli_real_escape_string($con, $_POST['lat1']);
+    $longitude = mysqli_real_escape_string($con, $_POST['lon1']);
+    $address = mysqli_real_escape_string($con, $_POST['address']);
+    $address = str_replace( $remove, "", $address );
+    $comment = mysqli_real_escape_string($con, $_POST['comment']);
+    $var = nl2br(htmlspecialchars(stripslashes($comment)));
+    $var = str_replace( $remove, "",$var );
+    $ans1 = mysqli_real_escape_string($con, $_POST['address1']);
+    $ans1 = str_replace( $remove, "", $ans1 );
+    $ans2 = mysqli_real_escape_string($con, $_POST['address2']);
+    $ans2 = str_replace( $remove, "",$ans2 );
+    $ans3 = mysqli_real_escape_string($con, $_POST['address3']);
+    $ans3 = str_replace( $remove, "",$ans3 );
+    $ans4 = mysqli_real_escape_string($con, $_POST['address4']);
+    $ans4 = str_replace( $remove, "", $ans4 );
+    $ans5 = mysqli_real_escape_string($con, $_POST['address5']);
+    $ans5  = str_replace( $remove, "", $ans5  );
+    $ans6 = mysqli_real_escape_string($con, $_POST['address6']);
+    $ans6 = str_replace( $remove, "", $ans6 );
+    $ans7 = mysqli_real_escape_string($con, $_POST['comments']);
+    $var1 = nl2br(htmlspecialchars(stripslashes($ans7)));
+    $var1  = str_replace( $remove, "", $var1  );
+    $ip= mysqli_real_escape_string($con, $_POST['ip']);
+    $entry_date = mysqli_real_escape_string($con, $_POST['date1']);
 
 
-if ( isset( $_SERVER["HTTP_CF_CONNECTING_IP"] ) ) {
- $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    if ( isset( $_SERVER["HTTP_CF_CONNECTING_IP"] ) ) {
+     $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+    }
+    if ( isset( $_SERVER["HTTP_X_FORWARDED_FOR"] ) ) {
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    if ( isset( $_SERVER["REMOTE_ADDR"] ) ) {
+      $ip = $_SERVER['REMOTE_ADDR'];
+    }
+
+
+    if(((!empty($latitude))&&(!empty($longitude)))&&((!empty($address))||(!empty($comment)) ||(empty($num_rows))))
+    {
+    $sql=("INSERT INTO SiteDetails (latitude,longitude,adress, owner,zoning,planningistory,size,derelict,heritage,desription,ip,Suggesteduses,Date_of_Entry)
+    VALUES ('$latitude', '$longitude','$address', '$ans1','$ans2','$ans3','$ans4','$ans5','$ans6','$var','$ip','$var1','$entry_date')");
+
+    if (!mysqli_query($con,$sql)){
+    $msg = "Data Not Updated Successfully!";
+    die('Error: ' . mysqli_error($con));
+
+
+    }
+
+    else
+    {
+
+    $msg = "Data Inserted Successfully!";
+    }
+    }
 }
-if ( isset( $_SERVER["HTTP_X_FORWARDED_FOR"] ) ) {
-  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-}
-if ( isset( $_SERVER["REMOTE_ADDR"] ) ) {
-  $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-
-if(((!empty($latitude))&&(!empty($longitude)))&&((!empty($address))||(!empty($comment)) ||(empty($num_rows))))
-{
-$sql=("INSERT INTO SiteDetails (latitude,longitude,adress, owner,zoning,planningistory,size,derelict,heritage,desription,ip,Suggesteduses,Date_of_Entry)
-VALUES ('$latitude', '$longitude','$address', '$ans1','$ans2','$ans3','$ans4','$ans5','$ans6','$var','$ip','$var1','$entry_date')");
-
-if (!mysqli_query($con,$sql)){
-$msg = "Data Not Updated Successfully!";
-die('Error: ' . mysqli_error($con));
-
-
-}
-
-else
-{
-
-$msg = "Data Inserted Successfully!";
-}
-}
-
-
-}
-
-
-
 
 // Check connection
-mysqli_close($con);
+if (isset($con))
+    mysqli_close($con);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -112,138 +108,137 @@ button.hover
 {background-color:#06F;
 }
 </style>
-</head><link href="css/bootstrap-responsive.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.css" rel="stylesheet">
 <link href="css/bootstrap.css" rel="stylesheet">
-</head>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 <script type="text/javascript">
-var map;
-var panorama;
-var x = 1;
-var sv = new google.maps.StreetViewService();
-var i ;
-var c ;
-function initialize() {
-var a = sessionStorage.getItem("lll");
-var b = sessionStorage.getItem("llll");
-c = sessionStorage.getItem("locat");
-document.getElementById("lat").value = a;
-document.getElementById("lon").value = b;
+    var map;
+    var panorama;
+    var x = 1;
+    var sv = new google.maps.StreetViewService();
+    var i ;
+    var c ;
+    function initialize() {
+    var a = sessionStorage.getItem("lll");
+    var b = sessionStorage.getItem("llll");
+    c = sessionStorage.getItem("locat");
+    document.getElementById("lat").value = a;
+    document.getElementById("lon").value = b;
 
-var today = new Date();
-var dd = today.getDate();
-var mm = today.getMonth() + 1; //January is 0!
-var yyyy = today.getFullYear();
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
 
-if(dd<10) {
-dd='0'+dd
-}
+    if(dd<10) {
+    dd='0'+dd
+    }
 
-if(mm<10) {
-mm='0'+mm
-}
+    if(mm<10) {
+    mm='0'+mm
+    }
 
-today = mm+'/'+dd+'/'+yyyy;
-document.getElementById("date1").value = today;
+    today = mm+'/'+dd+'/'+yyyy;
+    document.getElementById("date1").value = today;
 
-if(b.length<6)
-{
-alert('Invalid Latitude and Longitude');
-document.getElementById("lon").value = "";
-document.getElementById("lat").value = "";
-}
-val = parseFloat(b);
-if(isNaN(val))
-{document.getElementById("lon").value = "";
-alert('Invalid Latitude and Longitude');
-}
-else
-{
-document.getElementById("lon").value = b;
-}
-document.getElementById("lat").value = a;
-document.body.scroll = "yes"
-var astorPlace = new google.maps.LatLng(a,b);
+    if(b.length<6)
+    {
+    alert('Invalid Latitude and Longitude');
+    document.getElementById("lon").value = "";
+    document.getElementById("lat").value = "";
+    }
+    val = parseFloat(b);
+    if(isNaN(val))
+    {document.getElementById("lon").value = "";
+    alert('Invalid Latitude and Longitude');
+    }
+    else
+    {
+    document.getElementById("lon").value = b;
+    }
+    document.getElementById("lat").value = a;
+    document.body.scroll = "yes"
+    var astorPlace = new google.maps.LatLng(a,b);
 
-// Set up the map
-var mapOptions = {
-center: astorPlace,
-zoom: 18,
-streetViewControl:false,
-scrollwheel: false
-};
+    // Set up the map
+    var mapOptions = {
+    center: astorPlace,
+    zoom: 18,
+    streetViewControl:false,
+    scrollwheel: false
+    };
 
-map = new google.maps.Map(document.getElementById('map-canvas'),
-mapOptions);
-
-
-panorama = new google.maps.StreetViewPanorama(document.getElementById('map-canv'));
-sv.getPanoramaByLocation(astorPlace, 50, processSVData);
-
-}
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
 
 
-function processSVData(data, status) {
-if (status == google.maps.StreetViewStatus.OK) {
-var marker = new google.maps.Marker({
-position: data.location.latLng,
-map: map,
-title: data.location.description
-});
+    panorama = new google.maps.StreetViewPanorama(document.getElementById('map-canv'));
+    sv.getPanoramaByLocation(astorPlace, 50, processSVData);
 
-panorama.setPano(data.location.pano);
-panorama.setPov({
-heading: 270,
-pitch: 0
-});
-panorama.setVisible(true);
+    }
 
-google.maps.event.addListener(map, 'onload', function() {
 
-var markerPanoID = data.location.pano;
-// Set the Pano to use the passed panoID
-panorama.setPano(markerPanoID);
-panorama.setPov({
-heading: 270,
-pitch: 0
-});
-panorama.setVisible(true);
-});
-}
-}
+    function processSVData(data, status) {
+    if (status == google.maps.StreetViewStatus.OK) {
+    var marker = new google.maps.Marker({
+    position: data.location.latLng,
+    map: map,
+    title: data.location.description
+    });
 
-function goback()
-{
+    panorama.setPano(data.location.pano);
+    panorama.setPov({
+    heading: 270,
+    pitch: 0
+    });
+    panorama.setVisible(true);
 
-this.close();
-}
+    google.maps.event.addListener(map, 'onload', function() {
 
-function show()
-{
-alert('Data Uploading..Wait for a Few Seconds!');
-}
-function myFunction1()
-{
-window.open("Insert7.php",'Ratting',"height=700,width=700,scrollbars=1");
+    var markerPanoID = data.location.pano;
+    // Set the Pano to use the passed panoID
+    panorama.setPano(markerPanoID);
+    panorama.setPov({
+    heading: 270,
+    pitch: 0
+    });
+    panorama.setVisible(true);
+    });
+    }
+    }
 
-}
-function myFunction()
-{
+    function goback()
+    {
 
-window.open("Insert5.php",'Ratting',"height=700,width=700,scrollbars=1");
+    this.close();
+    }
 
-}
-function cll()
-{
-this.close();
-}
+    function show()
+    {
+    alert('Data Uploading..Wait for a Few Seconds!');
+    }
+    function myFunction1()
+    {
+    window.open("Insert7.php",'Ratting',"height=700,width=700,scrollbars=1");
+
+    }
+    function myFunction()
+    {
+
+    window.open("Insert5.php",'Ratting',"height=700,width=700,scrollbars=1");
+
+    }
+    function cll()
+    {
+    this.close();
+    }
 </script>
-<body onload="initialize()" style="background-color:#f4e851">
-    <?php
-    require_once('includes/header.php');
-    ?>
+
+<?php
+require_once('includes/head.php');
+require_once('includes/header.php');
+?>
 
 <form name="form1"  action="Entry_Form.php" method="post"  enctype="multipart/form-data" autocomplete="off">
 <!-- Fixed navbar -->
@@ -253,7 +248,10 @@ this.close();
 <input type="hidden" name="date1" ID="date1"  size="40"><br><br/><br/>
 <div style="width:100%;margin-right:80px;border:none;background-color:#FFF;padding:0px;background-color:transparent;border:none;" class="well sidebar-nav">
 <div style="margin-left:100px;margin-top:0px;font-family: Arial, Helvetica, sans-serif;font-weight:bold;color:#960;font-weight:1000;font-size:30px;">
-<?php echo $msg ?>
+<?php 
+if (isset($msg))
+    echo $msg;
+?>
 </div>
 <font style="font-family:'Source Sans Pro', sans-serif;font-size:28px;font-weight:bold;"><b>Add a space</font>
 <br/>
