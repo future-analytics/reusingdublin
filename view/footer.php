91ef568e2593f639382501b5d1b81cfb14afb939
Daithi Coombes <webeire@gmail.com>
2015-05-19 23:13:43 +0100

    bootstrap concept design for navbar

--- view/footer.php
+++ view/footer.php
@@ -3,6 +3,7 @@
 * @author  daithi coombes <david.coombes@futureanalytics.ie>
 */
 ?>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
--- view/header.php
+++ view/header.php
@@ -1,16 +1,37 @@
 <?php
 /**
- * The header and nav view file.
- * @author daithi coombes <david.coombes@futureanalytics.ie>
- */
+* The header and nav view file.
+* @package ReusingDublin
+* @author daithi coombes <david.coombes@futureanalytics.ie>
+*/
 ?>
-
-<nav class="navbar navbar-default">
+<nav id="mainNav" class="navbar navbar-default">
     <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-        <a class="navbar-brand" href="#">
-            <img alt="Brand" src="/images/logo.png">
-        </a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
         </div>
-    </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#home">HOME<span class="sr-only">(current)</span></a></li>
+                <li><a href="#how-it-works">HOW IT WORKS</a></li>
+                <li><a href="#try-it-out">TRY IT OUT</a></li>
+                <li><a href="#about">ABOUT</a></li>
+                <li><a href="#mailing-list">MAILING LIST</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right" id="social-links">
+                <li><a href="#">facebook</a></li>
+                <li><a href="#">email</a></li>
+                <li><a href="#">instagram</a></li>
+                <li><a href="#">twitter</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
