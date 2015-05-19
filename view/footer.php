b6e9b4f942273f48fd06e80e7e8a161deb75edb0
Daithi Coombes <webeire@gmail.com>
2015-05-19 22:51:52 +0100

    clean include files

--- view/footer.php
+++ view/footer.php
@@ -3,35 +3,7 @@
 * @author  daithi coombes <david.coombes@futureanalytics.ie>
 */
 ?>
-<section id="menu5" class="container"  style="padding-top:60px;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" align="center">
-	<p>Subscribe to our mailing list to get notification about</br>
-	upcoming news, latest project activities and much more!</p>
-<div class="container" style="float:center; margin-top:30px;" >
-		<input type="email" class="abc" name='email' style="background-color:#00afc9;width:60%;height:40px;border:none;font-family:'Source Sans Pro', sans-serif;font-size:20px;font-weight:regular;" id='email1' placeholder="Your Email Address">
-		<input type="submit" id="subscribe" style="background-color:#F00;color:white;height:40px;border:none;font-family:'Source Sans Pro', sans-serif;font-size:17px;font-weight:600;width:39%;margin-left:-3px;" name="added" class="subS" value="SUBSCRIBE" onClick="javascript:ValidateEmail();"/>
-	</div>
-	<div class="container" style="float:center; margin-top:40px;">
-		<img  src="rd_logo.png" style="float:!important;margin-top:20px;">
-		<img  src="Eu_logo1.png" style="float:!important;margin-left:30px;margin-top:20px;" >
-		<img  src="images/turas.png"  style="float:!important; margin-left:30px;margin-top:20px;">
-	</div>
-</section>
-
-<div class="container" style="text-align:center; margin-top:30px;" >
-	<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Reusing Dublin is licensed under &nbsp;<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" /></a><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title"></a>.
-
-
-<!-- Bootstrap core JavaScript--->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-		<script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
-		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-
-
-		<script type="text/javascript" src="js/jquery.mobile.customized.min.js"></script>
-		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-		<script src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
-	
-	    <script src="js/global.js" type="text/javascript"></script>	
+	    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 		<script type="text/javascript">
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
