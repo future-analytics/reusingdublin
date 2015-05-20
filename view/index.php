998946f699e4534d1d0acfe0c807a4ed9ef0a39c
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-20 14:37:43 +0100

    sperate logic from view files
    Signed-off-by: Daithi Coombes <david.coombes@futureanalytics.ie>

--- view/index.php
+++ view/index.php
@@ -4,16 +4,6 @@
  * @package ReusingDublin
  * @author daithi coombes <david.coombes@futureanalytics.ie>
  */
-
-$config = Config::getInstance();
-
-$data = (object) array(
-	'title' => 'Home',
-);
-
-require_once('head.php');
-require_once('header.php');
-	?>
+?>
 	<h1>Index.php</h1>
-	<?php
-require_once('footer.php');
+<?php
