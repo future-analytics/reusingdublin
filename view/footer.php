f8c91b1aabcc204039572d233c4c7efa366e43b1
Daithi Coombes <webeire@gmail.com>
2015-05-20 01:03:18 +0100

    fix footer
    Signed-off-by: Daithi Coombes <webeire@gmail.com>

--- view/footer.php
+++ view/footer.php
@@ -3,18 +3,31 @@
 * @author  daithi coombes <david.coombes@futureanalytics.ie>
 */
 ?>
-			</div><!-- close row //-->
-		</div><!-- close container //-->
+        </div>
+        </div>
+    `   <footer class="footer">
+            <div class="row affiliates">
+                <img src="/assets/images/logo-185x65.png" alt="reusing dublin logo"/> 
+                <img src="/assets/images/logo-eu.png" alt="EU Logo">
+                <img src="/assets/images/logo-turas.png">
+            </div>
+            <div class="row license">
+                <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">
+                    Reusing Dublin is licensed under
+                    <img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png">
+                </a>
+            </div>
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-		<script type="text/javascript">
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-59451880-1', 'auto');
-			ga('send', 'pageview');
-		</script>
-	</body>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+                <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+                <script type="text/javascript">
+                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                    ga('create', 'UA-59451880-1', 'auto');
+                    ga('send', 'pageview');
+                </script>
+        </footer>
+    </body>
 </html>
