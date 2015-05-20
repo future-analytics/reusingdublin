1eba80e51b11f9f545d89d979a1e4bb6d9538515
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-20 14:28:55 +0100

    decouple config from bootstrap

--- index.php
+++ index.php
@@ -10,9 +10,31 @@
  */
 define('REUSINGDUBLIN_DIR',		realpath(dirname(__FILE__)));
 define('REUSINGDUBLIN_DEBUG',	1);
+// end constants
 
-// boostrap
+
+/**
+ * Configuration
+ */
+new Config(require_once(REUSINGDUBLIN_DIR . '/config.php'));
+
+$config = Config::getInstance();
+$query = (isset($_GET['q'])) ?
+	$_GET['q'] :
+	null;
+$config->set('query', $query);
+// end Configuration
+
+
+/**
+ * boostrap
+ */
 require_once('bootstrap.php');
+// end bootstrap
 
-// print view
+
+/**
+ * Print View
+ */
 ReusingDublin\View::getView($config->routes[0]);
+// end Print View
