790e35c1678ccfd60bf78c45f32d11b6e84277ed
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-21 10:50:04 +0100

    mailing list concept

--- view/index.php
+++ view/index.php
@@ -90,3 +90,41 @@
 
 			</div>
 		</section>
+
+		<section id="about" class="container">
+			<div class="row">
+
+				<div class="col-xs-6">
+					<p class="title">About the Project</p>
+					<p>Reusing Dublin responds to the observation that we don’t use space in our city efficiently - and sometimes we don’t use it at all.</p>
+					<p>Reusing Dublin attempts to map underused spaces in order to identify opportunities for using the city more efficiently for the benefit of everyone.</p>
+					<p>Underused spaces include sites and buildings that are not used at all (vacant) or that are only partly in use. It also includes spaces that may have a use, like a roof or a grassed area, but that could accommodate additional uses.</p>
+					<p>Re-using Dublin is an experimental research project that is part of a wider EU FP7 project called TURAS (Transitioning towards Urban Resilience and Sustainability).</p>
+				</div>
+
+			</div>
+		</section>
+
+		<section id="mailing-list" class="container">
+
+			<div class="row">
+				<div class="col-xs-offset-3 col-xs-6 text-center">
+					<p>
+						Subscribe to our newsletter email to get notification about
+						upcoming news, latest project activities and much more!
+					</p>
+				</div>
+			</div>
+
+			<div class="row">
+				<form id="maillist">
+					<div class="col-xs-8">
+						<input type="email" name="data[email]" placeholder="Your email address">
+					</div>
+					<div class="col-xs-4">
+						<input type="submit" value="SUBSCRIBE">
+					</div>
+				</form>
+			</div>
+
+		</section>
