e00040c44c21b84403ee0c7818ae8908fd66b615
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-21 12:39:17 +0100

    scrolling animation

--- view/footer.php
+++ view/footer.php
@@ -3,9 +3,8 @@
 * @author  daithi coombes <david.coombes@futureanalytics.ie>
 */
 ?>
-        </div>
-        </div>
-    `   <footer class="footer">
+
+        <footer class="footer">
             <div class="row affiliates">
                 <img src="/assets/images/logo-185x65.png" alt="reusing dublin logo"/> 
                 <img src="/assets/images/logo-eu.png" alt="EU Logo">
@@ -18,16 +17,17 @@
                 </a>
             </div>
 
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-                <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-                <script type="text/javascript">
-                    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-                    ga('create', 'UA-59451880-1', 'auto');
-                    ga('send', 'pageview');
-                </script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+            <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+            <script src="/assets/js/reusingdublin.js" type="text/javascript"></script>
+            <script type="text/javascript">
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                ga('create', 'UA-59451880-1', 'auto');
+                ga('send', 'pageview');
+            </script>
         </footer>
     </body>
 </html>
