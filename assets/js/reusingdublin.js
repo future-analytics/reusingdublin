b908fa4196e36d1cd56d91f63bc6602d1e8e4e2e
Daithi Coombes <david.coombes@futureanalytics.ie>
2015-05-21 12:39:17 +0100

    scrolling animation

---
+++ assets/js/reusingdublin.js
@@ -0,0 +1,49 @@
+/**
+ * Onload function
+ * @author daithi coombes <david.coombes@futureanalytics.ie>
+ */
+$(document).ready(function(){
+
+    reusingdublin.init();
+});
+
+/**
+ * The main class for the ReusingDublin site.
+ * @class ReusingDublin
+ */
+var ReusingDublin = function(){
+
+    /**
+     * @constructor
+     * @member {ReusingDublin}
+     * @return {ReusingDublin} Returns self.
+     */
+    this.init = function(){
+
+        $("#mainNav ul li a[href^='#']")
+            .on('click', this.scrollTo);
+
+        return this;
+    }
+
+    /**
+     * Animate homepage scroll
+     * @param  {event}
+     * @return {ReusingDublin}   Returns self.
+     */
+    this.scrollTo = function(e) {
+
+        e.preventDefault();
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 700, function(){
+            window.location.hash = hash;
+        });
+
+        return this;
+    }
+}
+var reusingdublin = new ReusingDublin();
